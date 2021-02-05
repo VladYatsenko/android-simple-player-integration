a719c625303e00a613423b2018bfde1c9f75c1ee
Vlad <yatsenko.dev@gmail.com>
2021-02-05 17:59:00 +0200

    started music service

--- app/src/main/java/com/yatsenko/simpleplayer/player/exoplayer/ExoPlayerHolder.kt
+++ app/src/main/java/com/yatsenko/simpleplayer/player/exoplayer/ExoPlayerHolder.kt
@@ -14,7 +14,6 @@ import com.google.android.exoplayer2.source.smoothstreaming.DefaultSsChunkSource
 import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
@@ -22,9 +21,8 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import com.yatsenko.simpleplayer.R
 import com.yatsenko.simpleplayer.player.abstraction.model.MediaResource
-import com.yatsenko.simpleplayer.player.abstraction.PlayerAdapter
+import com.yatsenko.simpleplayer.player.exoplayer.utils.AudioFocusWrapper
 import com.yatsenko.simpleplayer.player.exoplayer.utils.ExoPlayerAdapter
-import com.yatsenko.simpleplayer.player.google.AudioFocusWrapper
 
 class ExoPlayerHolder constructor(private val context: Context) : ExoPlayerAdapter {
 
